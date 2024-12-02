28084121d905b7c0522669af2afea199746c7d02
md rayhan <jaber.ac1983@gmail.com>
2024-12-02 10:11:11 +0600

    setup complete

--- phonebook.py
+++ phonebook.py
@@ -1,5 +1,19 @@
 from tkinter import *
 
+# all colors
+c0 = "#ffffff"
+c1 = "#000000"
+c2 = "#4456f0"
+
 win = Tk()
+win.title('PhoneBook | Md Rayhan Munshi')
+win.geometry('485x450')
+win['bg'] = c0
+win.resizable(width=False,height=False)
+
+
+
+
+
 
 win.mainloop()
